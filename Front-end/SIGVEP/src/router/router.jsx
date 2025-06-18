402137f7f96a80a58df698468e027697bde1ad97
PWM-Alex <VictorAlex1233@hotmail.com>
2025-06-18 08:56:59 -0600

    Proyecto Front-End

---
+++ Front-end/SIGVEP/src/router/router.jsx
@@ -0,0 +1,11 @@
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
+
+const AppRouter = () => {
+    return(
+        <Routes>
+        
+        </Routes>
+    )
+}
+
+export default AppRouter
